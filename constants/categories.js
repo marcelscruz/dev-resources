a6971fffeedcff2c37d8c56c999ccd169b37e89f
Marcel <marcelcruz@live.com>
2021-01-25 16:44:30 -0300

    Add constants

---
+++ constants/categories.js
@@ -0,0 +1,80 @@
+module.exports = {
+    assets: {
+        fonts: 'Assets/Fonts',
+        icons: 'Assets/Icons',
+        illustrations: 'Assets/Illustrations',
+        images: 'Assets/Images',
+        logos: 'Assets/Logos',
+        videos: 'Assets/Videos',
+    },
+
+    blockchain: {
+        podcasts: 'Blockchain/Podcasts',
+    },
+
+    indieHacking: {
+        books: 'Indie Hacking/Books',
+        platforms: 'Indie Hacking/Platforms',
+        podcasts: 'Indie Hacking/Podcasts',
+        utilities: 'Indie Hacking/Utilities',
+    },
+
+    infrastructure: {
+        hosting: 'Infrastructure/Hosting',
+    },
+
+    jobs: {
+        boards: 'Jobs/Boards',
+        freelancing: 'Jobs/Freelancing',
+        interviews: 'Jobs/Interviews',
+        remoteOnly: 'Jobs/Remote Only',
+        resumeBuilders: 'Jobs/Resume Builders',
+    },
+
+    programming: {
+        animations: 'Programming/Animations',
+        books: 'Programming/Books',
+        charts: 'Programming/Charts',
+        codeChallenges: 'Programming/Code Challenges',
+        codeSnippets: 'Programming/Code Snippets',
+        databases: 'Programming/Databases',
+        docsAndCheatsheets: 'Programming/Docs & Cheatsheets',
+        javascript: {
+            react: 'Programming/JavaScript/React',
+        },
+        learn: 'Programming/Learn',
+        newsletters: 'Programming/Newsletters',
+        podcasts: 'Programming/Podcasts',
+        templates: 'Programming/Templates',
+        Testing: 'Programming/Testing',
+        uiKitsAndLibraries: 'Programming/UI Kits & Libraries',
+        youtubeChannels: 'Programming/YouTube Channels',
+    },
+
+    readingAndWriting: {
+        learn: 'Reading & Writing/Learn',
+        platforms: 'Reading & Writing/Platforms',
+        utilities: 'Reading & Writing/Utilities',
+    },
+
+    toolsAndUtilities: {
+        accessibility: 'Tools & Utilities/Accessibility',
+        analytics: 'Tools & Utilities/Analytics',
+        browsersAndExtensions: 'Tools & Utilities/Browsers & Extensions',
+        editorsAndExtensions: 'Tools & Utilities/Editors & Extensions',
+        general: 'Tools & Utilities/General',
+        imageAndVideoEditing: 'Tools & Utilities/Image & Video Editing',
+        performance: 'Tools & Utilities/Performance',
+        typing: 'Tools & Utilities/Typing',
+        websiteBuilders: 'Tools & Utilities/Website Builders',
+    },
+
+    uiDesign: {
+        colorPalettes: 'UI Design/Color Palettes',
+        inspiration: 'UI Design/Inspiration',
+        learn: 'UI Design/Learn',
+        prototyping: 'UI Design/Prototyping',
+        utilities: 'UI Design/Utilities',
+        youtubeChannels: 'UI Design/YouTube Channels',
+    },
+}
---
+++ constants/links.js
@@ -0,0 +1,18 @@
+module.exports = {
+    main: 'Main',
+
+    // Platforms
+    applePodcasts: 'Apple Podcasts',
+    facebook: 'Facebook',
+    github: 'GitHub',
+    googlePodcasts: 'Google Podcasts',
+    indieHackers: 'Indie Hackers',
+    instagram: 'Instagram',
+    linkedin: 'LinkedIn',
+    npm: 'npm',
+    productHunt: 'Product Hunt',
+    spotify: 'Spotify',
+    twitch: 'Twitch',
+    twitter: 'Twitter',
+    youtube: 'YouTube',
+}
