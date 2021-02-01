ce7befdfbcda5c9d2b992f1093edcd76cd9e9d19
Marcel <marcelcruz@live.com>
2021-02-01 15:56:43 -0300

    Fix README logo anchor

--- utils/readme/update-readme.js
+++ utils/readme/update-readme.js
@@ -4,7 +4,8 @@ const createIndex = require('./create-index')
 const createTables = require('./create-tables')
 const writeToFile = require('../write-to-file')
 
-const logo = '[![Dev Resources](./public/assets/images/logo-with-bg.png | width=500)](https://devresourc.es)'
+const logo =
+    '<a href="https://devresourc.es"><p align="center"><img width="500" src="./public/assets/images/logo-with-bg.png" /></p></a>\n\n'
 
 const description = 'The list and public API of the resources that power [Dev Resources](https://devresourc.es).\n\n'
 
