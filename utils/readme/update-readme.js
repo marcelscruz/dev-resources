94f9475d573dd6179541b4a19661b8e6dcc12c52
Marcel <marcelcruz@live.com>
2021-02-01 15:54:11 -0300

    Add link on README logo

--- utils/readme/update-readme.js
+++ utils/readme/update-readme.js
@@ -4,7 +4,7 @@ const createIndex = require('./create-index')
 const createTables = require('./create-tables')
 const writeToFile = require('../write-to-file')
 
-const logo = '<p align="center"><img width="500" src="./public/assets/images/logo-with-bg.png" /></p>\n\n'
+const logo = '[![Dev Resources](./public/assets/images/logo-with-bg.png | width=500)](https://devresourc.es)'
 
 const description = 'The list and public API of the resources that power [Dev Resources](https://devresourc.es).\n\n'
 
