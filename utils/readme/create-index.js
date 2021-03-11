9514b6abe81b22f9370699af75378debf043e2d4
Marcel <marcelcruz@live.com>
2021-03-11 16:37:37 -0300

    Format index lists

--- utils/readme/create-index.js
+++ utils/readme/create-index.js
@@ -9,21 +9,21 @@ function createLinks(nodes) {
         let credit = ''
 
         if (isPublicApis(id)) {
-            credit = ` - Powered by [Public APIs](https://github.com/public-apis/public-apis)`
+            credit = ` - Powered by <a href="https://github.com/public-apis/public-apis">Public APIs</a>`
         }
 
-        return `<li id="${id}">${name}${credit}</li>`
+        return `<li id="${id}"><a href="#${id}">${name}${credit}</a></li>`
     } else {
         const { name, id } = nodes
 
         const children = nodes.children.map(createLinks).join('')
 
-        return `<details><summary id="${id}">${name}</summary><ul>${children}</ul></details>`
+        return `<li style="margin: 0px !important;"><details style="margin: 0px !important;"><summary id="${id}">${name}</summary><ul style="list-style-type: none;">${children}</ul></details></li>`
     }
 }
 
 module.exports = function (resources) {
     const links = resources.map(createLinks).join('')
 
-    return `## Index\n${links}`
+    return `## Index\n<ul style="list-style-type: none; padding: 0;">${links}</ul>`
 }
