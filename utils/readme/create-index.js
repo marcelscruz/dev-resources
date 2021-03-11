e9b85a7848da0122f99a2ddb0d7655c4fa0e0789
Marcel <marcelcruz@live.com>
2021-03-11 16:57:24 -0300

    Formatting stuff, again

--- utils/readme/create-index.js
+++ utils/readme/create-index.js
@@ -12,14 +12,14 @@ function createLinks(nodes) {
             credit = ` - Powered by <a href="https://github.com/public-apis/public-apis">Public APIs</a>`
         }
 
-        return `<br/><a href="#${id}">${name}${credit}</a>`
-        // return `<li id="${id}"><a href="#${id}">${name}${credit}</a></li>`
+        // return `<br/><a href="#${id}">${name}${credit}</a>`
+        return `<li id="${id}"><a href="#${id}">${name}${credit}</a></li>`
     } else {
         const { name, id } = nodes
 
         const children = nodes.children.map(createLinks).join('')
 
-        return `<br/><details><summary id="${id}">${name}</summary>${children}</details>`
+        return `<details><summary id="${id}">${name}</summary>${children}</details><br/>`
     }
 }
 
