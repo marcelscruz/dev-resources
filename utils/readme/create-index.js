b83537cda8012484c27e4984775d32166b284c38
Marcel <marcelcruz@live.com>
2021-03-11 16:15:01 -0300

    Add collapse on index parent items

--- utils/readme/create-index.js
+++ utils/readme/create-index.js
@@ -16,13 +16,16 @@ function createLinks(nodes) {
             credit = ` - Powered by [Public APIs](https://github.com/public-apis/public-apis)`
         }
 
-        return `\n${createIndentation(depth)}* [${name}](#${id})${credit}`
+        return `\n<li id="${id}">${name}${credit}</li>`
+        // return `\n${createIndentation(depth)}* [${name}](#${id})${credit}`
     } else {
         const { name, depth, id } = nodes
 
         const children = nodes.children.map(createLinks).join('')
 
-        return `\n${createIndentation(depth)}* [${name}](#${id}) ${children}`
+        return `\n<details><br/><summary id="${id}">${name}</summary><br/><br/><ul>${children}</ul><br/></details>`
+
+        // return `\n${createIndentation(depth)}* [${name}](#${id}) ${children}`
     }
 }
 
