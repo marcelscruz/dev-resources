941ee261a3b7e2a9967c9c644cd4cc18e01861f3
Marcel <marcelcruz@live.com>
2021-03-11 16:59:33 -0300

    🙏

--- utils/readme/create-index.js
+++ utils/readme/create-index.js
@@ -12,14 +12,13 @@ function createLinks(nodes) {
             credit = ` - Powered by <a href="https://github.com/public-apis/public-apis">Public APIs</a>`
         }
 
-        // return `<br/><a href="#${id}">${name}${credit}</a>`
         return `<li id="${id}"><a href="#${id}">${name}${credit}</a></li>`
     } else {
         const { name, id } = nodes
 
         const children = nodes.children.map(createLinks).join('')
 
-        return `<details><summary id="${id}">${name}</summary>${children}</details><br/>`
+        return `<details><summary id="${id}">${name}</summary>${children}</details>`
     }
 }
 
