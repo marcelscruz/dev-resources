bea96d4a5ec194ff536795674c8e593e7bd0f53e
Luke Haas <lukehaas7@gmail.com>
2022-11-24 23:27:46 +0000

    add runjs (#300)
    * add runjs
    
    * add runjs
    
    * update links

--- resources/r.ts
+++ resources/r.ts
@@ -435,6 +435,19 @@ export const resources: Resource[] = [
         icon: 'https://www.jetbrains.com/favicon-32x32.png',
     },
     {
+        name: 'RunJS',
+        categories: [toolsAndUtilities.editorsAndExtensions],
+        description: 'RunJS is a desktop JavaScript playground that evaluates your code as you type and gives instant feedback. Ideal for prototyping ideas or trying out new libraries.',
+        links: {
+            [website]: 'https://runjs.app',
+            [twitter]: 'https://twitter.com/runjs_app',
+            [instagram]: 'https://instagram.com/runjs_app',
+            [github]: 'https://github.com/lukehaas/RunJS'
+        },
+        icon: 'https://raw.githubusercontent.com/lukehaas/RunJS/master/docs/icon-small.png',
+        keywords: ['javascript', 'typescript', 'playground', 'node', 'repl', 'sandbox', 'development']
+    },
+    {
         name: 'Rust Jobs',
         categories: [jobs.boards],
         description:
