3d85999c273f581490f4d5df2ee7672fa1afee09
Marcel Cruz <12413903+marcelscruz@users.noreply.github.com>
2023-05-10 10:42:56 +0200

    Remove Atom

--- resources/a.ts
+++ resources/a.ts
@@ -216,14 +216,6 @@ export const resources: Resource[] = [
         icon: 'https://www.artify.co/images/landing/favicon.png',
     },
     {
-        name: 'Atom',
-        categories: ['Editor', 'Programming'],
-        description:
-            'At GitHub, we’re building the text editor we’ve always wanted: hackable to the core, but approachable on the first day without ever touching a config file. We can’t wait to see what you build with it.',
-        url: 'https://atom.io/',
-        icon: 'https://atom.io/favicon.ico',
-    },
-    {
         name: 'Authentic Jobs',
         categories: ['Job'],
         description: 'The leading job board for designers, developers, and creative pros.',
