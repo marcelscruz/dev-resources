18506e482241e02f2811ef988de4d5d6c5906d95
Marcel Cruz <12413903+marcelscruz@users.noreply.github.com>
2024-05-13 16:59:21 +0200

    Add Zenlogin as sponsor

--- README.md
+++ README.md
@@ -4,7 +4,11 @@
 
  <div align="center">Sponsored by</div><br/>
 
-<p align="center"><a href="https://gcore.com?utm_source=public_apis&utm_content=dev_resources_repo"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/gcore-dark-mode.png" width="180px"><source media="(prefers-color-scheme: light)" srcset="./assets/gcore-light-mode.png" width="180px"><img alt="Gcore logo" src="./assets/gcore-dark-mode.png" width="180px"></picture></a></p><br/>
+<p align="center"><a href="https://gcore.com?ref=dev_resources&utm_content=dev_resources_repo"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/gcore-dark-mode.png" width="180px"><source media="(prefers-color-scheme: light)" srcset="./assets/gcore-light-mode.png" width="180px"><img alt="Gcore logo" src="./assets/gcore-dark-mode.png" width="180px"></picture></a></p><br/>
+
+
+
+<p align="center"><a href="https://zenlogin.co?ref=dev_resources&utm_content=dev_resources_repo"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/zenlogin.svg" width="210px"><source media="(prefers-color-scheme: light)" srcset="./assets/zenlogin.svg" width="210px"><img alt="Zenlogin logo" src="./assets/zenlogin.svg" width="210px"></picture></a></p>
 
  ---
 >❗️ This `README.md` file and the `/db` folder are auto-generated, so please ***do not*** edit them. Changes related to resources should happen within the `/resources` folder.
--- utils/readme/update-readme.js
+++ utils/readme/update-readme.js
@@ -12,10 +12,11 @@ const description = `<div align="center">The resources that power <a href="${url
 
 const logoDR = `<a><p align="center">[<img src="./assets/logo.png">](${url})</p></a>\n\n`
 
-const logoGcore = `<p align="center"><a href="https://gcore.com?utm_source=public_apis&utm_content=dev_resources_repo"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/gcore-dark-mode.png" width="180px"><source media="(prefers-color-scheme: light)" srcset="./assets/gcore-light-mode.png" width="180px"><img alt="Gcore logo" src="./assets/gcore-dark-mode.png" width="180px"></picture></a></p>`
+const logoGcore = `<p align="center"><a href="https://gcore.com?ref=dev_resources&utm_content=dev_resources_repo"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/gcore-dark-mode.png" width="180px"><source media="(prefers-color-scheme: light)" srcset="./assets/gcore-light-mode.png" width="180px"><img alt="Gcore logo" src="./assets/gcore-dark-mode.png" width="180px"></picture></a></p>`
+const logoZenlogin = `<p align="center"><a href="https://zenlogin.co?ref=dev_resources&utm_content=dev_resources_repo"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/zenlogin.svg" width="210px"><source media="(prefers-color-scheme: light)" srcset="./assets/zenlogin.svg" width="210px"><img alt="Zenlogin logo" src="./assets/zenlogin.svg" width="210px"></picture></a></p>`
 
-const sponsors = `<div align="center">Sponsored by</div><br/>\n\n${logoGcore}<br/>\n\n`
-// const sponsors = `<div align="center">Sponsored by</div><br/>\n\n${logoClerk}<br/>\n\n\n\n${logoGcore}\n\n`
+// const sponsors = `<div align="center">Sponsored by</div><br/>\n\n${logoGcore}<br/>\n\n`
+const sponsors = `<div align="center">Sponsored by</div><br/>\n\n${logoGcore}<br/>\n\n\n\n${logoZenlogin}\n\n`
 
 const warning =
     '---\n>❗️ This `README.md` file and the `/db` folder are auto-generated, so please ***do not*** edit them. Changes related to resources should happen within the `/resources` folder.\n---\n\n'
