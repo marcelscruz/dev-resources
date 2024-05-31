7c6e50a9415ed6896d95a0982ce51aa0c69a733f
Sergiu Chiriac <>
2024-05-31 21:00:32 +0100

    WLSpy

--- resources/w.ts
+++ resources/w.ts
@@ -225,6 +225,14 @@ export const resources: Resource[] = [
         url: 'https://www.wrappixel.com/',
     },
     {
+        name: 'WLSpy',
+        categories: ['Marketing', 'AI'],
+        description:
+            'WLSpy helps users discover winning products by analyzing market trends and competitor strategies.',
+        keywords: ['Product Research', 'Market Analysis', 'Competitor Monitoring'],
+        url: 'https://wlspy.com',
+    },
+    {
         name: 'Writty',
         categories: ['Writing'],
         description: 'A simple writing app and text editor to help you focus on what matters.',
