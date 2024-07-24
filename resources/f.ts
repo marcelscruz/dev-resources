381eeaf505c45d0c80659da796b7573318315a79
Marcel Cruz <12413903+marcelscruz@users.noreply.github.com>
2024-07-24 10:26:11 +0200

    Add Fusion

--- resources/f.ts
+++ resources/f.ts
@@ -628,4 +628,11 @@ export const resources: Resource[] = [
             'JavaScript charts for web and mobile apps. 95+ chart types, 1400+ maps and 20+ business dashboards with pre-built themes for any business use-case. Build fast, responsive and highly customizable data visualizations trusted by over 28,000 customers and 750,000 developers worldwide...',
         url: 'https://www.fusioncharts.com/',
     },
+    {
+        name: 'Fusion',
+        categories: ['API Building'],
+        description:
+            'Fusion is the "Notion-like" API client. Modular API design, testing & collaboration for the entire API lifecycle. Available as SaaS and self hosted.',
+        url: 'https://apyhub.com/product/fusion',
+    },
 ]
