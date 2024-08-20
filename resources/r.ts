a6c5fc6d3e2b12bc6939ed5fb32d39f4c80047c9
Anka <runner@Mac-1724156098264.local>
2024-08-20 12:22:57 +0000

    Auto updating db and README.md

--- resources/r.ts
+++ resources/r.ts
@@ -291,11 +291,10 @@ export const resources: Resource[] = [
             'The only online resume builder that’ll land you interviews. Create a professional resume in minutes, download, and print. Join 10 million happy job seekers.',
         url: 'https://resumegenius.com/',
     },
-        {
+    {
         name: 'Resume.io',
         categories: ['Resume'],
-        description:
-            'Helping millions of job-seekers worldwide create amazing professional resumes in minutes!',
+        description: 'Helping millions of job-seekers worldwide create amazing professional resumes in minutes!',
         url: 'https://resume.io',
     },
     {
