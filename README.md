b08cf1d8b9e1ff864acd699ac32f85b1e2ae6aaf
Marcel Cruz <12413903+marcelscruz@users.noreply.github.com>
2025-05-12 13:20:13 +0200

    chore: Add Apyhub and Supadata as sponsors

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 <!--🛑 👉 This README file and the /db folder are auto-generated, so please DON'T edit them. Changes related to resources should happen in the /resources folder. --> 
 
- <div align="center">Sponsored by</div> 
-    <br/>
-    <div>
+ <div align="center">Sponsored by</div><br/>
+
+<div>
+    <p align="center"><a href="https://apyhub.com?ref=dev_resources&utm_medium=repo_readme"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/apyhub-dark-mode.svg"         width="150px"><source media="(prefers-color-scheme: light)" srcset="./assets/apyhub-light-mode.svg" width="150px"><img alt="Scrapfly logo" src="./assets/apyhub-dark-mode.svg" width="150px"></picture></a></p>
+    <p align="center"><a href="https://apyhub.com?ref=dev_resources&utm_medium=repo_readme">Apyhub</a>: A Modern API Platform to streamline API Development, Testing and Management. Take your API game to the next level with ApyHub.</p>
+</div><br/>
+
+
+
+<div>
     <p align="center"><a href="https://scrapfly.io?ref=dev_resources&utm_medium=repo_readme"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/scrapfly-dark-mode.svg"         width="150px"><source media="(prefers-color-scheme: light)" srcset="./assets/scrapfly-light-mode.svg" width="150px"><img alt="Scrapfly logo" src="./assets/scrapfly-dark-mode.svg" width="150px"></picture></a></p>
     <p align="center"><a href="https://scrapfly.io?ref=dev_resources&utm_medium=repo_readme">Scrapfly</a> offers a top-tier Web Scraping API that simplifies scraping by handling real browser rendering, rotating proxies, and various fingerprints to overcome major anti-bot measures.</p>
+</div><br/>
+
+
+
+<div>
+    <p align="center"><a href="https://supadata.ai?ref=dev_resources&utm_medium=repo_readme"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/supadata-dark-mode.svg"         width="150px"><source media="(prefers-color-scheme: light)" srcset="./assets/supadata-light-mode.svg" width="150px"><img alt="Scrapfly logo" src="./assets/supadata-dark-mode.svg" width="150px"></picture></a></p>
+    <p align="center"><a href="https://supadata.ai?ref=dev_resources&utm_medium=repo_readme">Supadata</a> is one stop-shop API for makers to read web and YouTube content, ready for AI training and retrieval.</p>
 </div>
+
+
  <br/><hr/><br/><br/> <div align="center">
     <a href="API.md">API</a> •
     <a href="CONTRIBUTING.md">Contributing Guide</a> •
--- utils/readme/update-readme.js
+++ utils/readme/update-readme.js
@@ -24,17 +24,26 @@ const links = `<div align="center">
     <a href="LICENSE">License</a>
 </div>`
 
+const logoApyhub = `<div>
+    <p align="center"><a href="https://apyhub.com?ref=dev_resources&utm_medium=repo_readme"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/apyhub-dark-mode.svg"         width="150px"><source media="(prefers-color-scheme: light)" srcset="./assets/apyhub-light-mode.svg" width="150px"><img alt="Scrapfly logo" src="./assets/apyhub-dark-mode.svg" width="150px"></picture></a></p>
+    <p align="center"><a href="https://apyhub.com?ref=dev_resources&utm_medium=repo_readme">Apyhub</a>: A Modern API Platform to streamline API Development, Testing and Management. Take your API game to the next level with ApyHub.</p>
+</div>`
+
+const logoSupadata = `<div>
+    <p align="center"><a href="https://supadata.ai?ref=dev_resources&utm_medium=repo_readme"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/supadata-dark-mode.svg"         width="150px"><source media="(prefers-color-scheme: light)" srcset="./assets/supadata-light-mode.svg" width="150px"><img alt="Scrapfly logo" src="./assets/supadata-dark-mode.svg" width="150px"></picture></a></p>
+    <p align="center"><a href="https://supadata.ai?ref=dev_resources&utm_medium=repo_readme">Supadata</a> is one stop-shop API for makers to read web and YouTube content, ready for AI training and retrieval.</p>
+</div>`
+
 const logoScrapfly = `<div>
     <p align="center"><a href="https://scrapfly.io?ref=dev_resources&utm_medium=repo_readme"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/scrapfly-dark-mode.svg"         width="150px"><source media="(prefers-color-scheme: light)" srcset="./assets/scrapfly-light-mode.svg" width="150px"><img alt="Scrapfly logo" src="./assets/scrapfly-dark-mode.svg" width="150px"></picture></a></p>
     <p align="center"><a href="https://scrapfly.io?ref=dev_resources&utm_medium=repo_readme">Scrapfly</a> offers a top-tier Web Scraping API that simplifies scraping by handling real browser rendering, rotating proxies, and various fingerprints to overcome major anti-bot measures.</p>
 </div>`
 
-// const sponsors = `<div align="center">Sponsored by</div><br/>\n\n${logoScrapfly}<br/>\n\n`
-// const sponsors = `<div align="center">Sponsored by</div><br/>\n\n${logoScrapfly}<br/>\n\n\n\n${logoZenlogin}\n\n`
-const sponsors = `<div align="center">Sponsored by</div> 
-    <br/>
-    ${logoScrapfly}
-`
+const sponsors = `<div align="center">Sponsored by</div><br/>\n\n${logoApyhub}<br/>\n\n\n\n${logoScrapfly}<br/>\n\n\n\n${logoSupadata}\n\n\n`
+// const sponsors = `<div align="center">Sponsored by</div>
+//     <br/>
+//     ${logoScrapfly}
+// `
 
 async function updateReadme() {
     try {
