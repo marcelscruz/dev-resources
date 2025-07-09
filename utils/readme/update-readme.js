9ea1cd388a416d3d0e4ecd903ac7fba463320b6a
Marcel Cruz <12413903+marcelscruz@users.noreply.github.com>
2025-07-09 16:30:49 +0200

    Add SerpApi as sponsor

--- utils/readme/update-readme.js
+++ utils/readme/update-readme.js
@@ -25,8 +25,13 @@ const links = `<div align="center">
 </div>`
 
 const logoApyhub = `<div>
-    <p align="center"><a href="https://apyhub.com?ref=dev_resources&utm_medium=repo_readme"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/apyhub-dark-mode.svg" width="150px"><source media="(prefers-color-scheme: light)" srcset="./assets/apyhub-light-mode.svg" width="150px"><img alt="Scrapfly logo" src="./assets/apyhub-dark-mode.svg" width="150px"></picture></a></p>
+    <p align="center"><a href="https://apyhub.com?ref=dev_resources&utm_medium=repo_readme"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/apyhub-dark-mode.svg" width="150px"><source media="(prefers-color-scheme: light)" srcset="./assets/apyhub-light-mode.svg" width="150px"><img alt="ApyHub logo" src="./assets/apyhub-dark-mode.svg" width="150px"></picture></a></p>
     <p align="center"><a href="https://apyhub.com?ref=dev_resources&utm_medium=repo_readme">ApyHub</a>: A Modern API Platform to streamline API Development, Testing and Management. Take your API game to the next level with ApyHub.</p>
+</div>`
+
+const logoSerpApi = `<div>
+    <p align="center"><a href="https://serpapi.com?ref=dev_resources&utm_medium=repo_readme"><picture><source media="(prefers-color-scheme: dark)" srcset="./assets/serpapi-dark-mode.svg" width="150px"><source media="(prefers-color-scheme: light)" srcset="./assets/serpapi-light-mode.svg" width="150px"><img alt="SerpApi logo" src="./assets/serpapi-dark-mode.svg" width="150px"></picture></a></p>
+    <p align="center"><a href="https://serpapi.com?ref=dev_resources&utm_medium=repo_readme">SerpApi</a>'s Search API makes it easy and fast to scrape Google and other search engines.</p>
 </div>`
 
 const logoLightpost = `<div>
@@ -34,7 +39,7 @@ const logoLightpost = `<div>
     <p align="center"><a href="https://lightpost.ai?ref=dev_resources&utm_medium=repo_readme&via=dr-pa">Lightpost</a>: Fully automate or co-create your blogs and posts with an A.I. trained on your business. Plan, design, write and upload anywhere, while keeping full control.</p>
 </div>`
 
-const sponsors = `<div align="center">Sponsored by</div><br/>\n\n${logoApyhub}<br/>\n\n\n\n${logoLightpost}<br/>\n\n\n\n`
+const sponsors = `<div align="center">Sponsored by</div><br/>\n\n${logoApyhub}<br/>\n\n\n\n${logoSerpApi}<br/>\n\n\n\n${logoLightpost}<br/>\n\n\n\n`
 // const sponsors = `<div align="center">Sponsored by</div>
 //     <br/>
 //     ${logoScrapfly}
