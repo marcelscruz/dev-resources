e3b2070b668a4a9452d7532f9b84d86787742f46
iamalexissimon <alexissimon232@gmail.com>
2025-08-01 18:50:11 +0530

    Add Smartshort (#853)

--- resources/s.ts
+++ resources/s.ts
@@ -432,6 +432,13 @@ export const resources: Resource[] = [
         url: 'https://www.slowandsteadypodcast.com/',
     },
     {
+        name: 'Smartshort',
+        description: 'Text to video',
+        categories: ['Video', 'AI', 'Marketing'],
+        url: 'https://smartshort.co',
+        keywords: ['video generator', 'tiktok generator', 'short form content generator', 'text to short'],
+    },
+    {
         name: 'Smashicons',
         description: 'Download the world',
         categories: ['Icon'],
