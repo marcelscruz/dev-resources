7f77d978bfe395692fea8db542156830a16dea7a
Rodrigo Rocco <rocco.rodrigo@hotmail.com>
2025-09-02 15:13:47 +0200

    Update j.ts (#857)

--- resources/j.ts
+++ resources/j.ts
@@ -154,4 +154,13 @@ export const resources: Resource[] = [
         categories: ['Job', 'Remote'],
         url: 'https://justremote.co/',
     },
+    ,
+    {
+        name: 'JobBoardSearch 🔎',
+        categories: ['Jobs', 'Job boards'],
+        description:
+            'Meta directory of job boards with filters. Helping: Job seekers to find jobs, Job boards to promote, Recruiters to find the best candidates',
+        url: 'https://jobboardsearch.com/',
+        keywords: ['jobs', 'job boards'],
+    },
 ]
