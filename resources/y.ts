a6d33f9225a184da68d565996dcd1ddd2888f6e7
cyril <38669260+cyrilbois@users.noreply.github.com>
2026-02-04 11:32:37 +0100

    Add YAMLine (#972)
    * Add YAMLine resource
    
    Added YAMLine resource with details and URL.
    
    * Add keywords to YAMLine resource

--- resources/y.ts
+++ resources/y.ts
@@ -1,3 +1,11 @@
 import { Resource } from 'types'
 
-export const resources: Resource[] = []
+export const resources: Resource[] = [
+    {
+        name: 'YAMLine',
+        description: 'YAMLine is an online YAML toolbox featuring a linter, formatter, merger, AI fixer, converters, and a Kubernetes manifest validator.',
+        categories: ['Tooling'],
+        url: 'https://yamline.com/',
+        keywords: ['yaml', 'web', 'developer tools', 'tools'],
+    },
+]
