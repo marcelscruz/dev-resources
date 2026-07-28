6a713756d44e08d91806fc7253fb0ccf1c3e3ebb
Demorphed <demorphed6@gmail.com>
2026-07-28 02:17:23 -0400

    Add Planomy Tax Data

--- resources/p.ts
+++ resources/p.ts
@@ -225,6 +225,14 @@ export const resources: Resource[] = [
         keywords: ['icons', 'line icons', 'vector icons', 'iOS icons', 'Android icons'],
     },
     {
+        name: 'Planomy Tax Data',
+        description:
+            '2026 US federal tax brackets, retirement contribution limits, Social Security, Medicare IRMAA and RMD ages as JSON, each figure cited to its IRS/SSA/CMS source',
+        categories: ['Library', 'Open Source'],
+        url: 'https://github.com/demorphed6-lgtm/planomy-tax-data',
+        keywords: ['tax', 'irs', 'tax brackets', 'dataset', 'open data', 'retirement', '401k', 'ira', 'hsa'],
+    },
+    {
         name: 'Plausible',
         description:
             'Plausible is a lightweight and open-source web analytics tool. Your website data is 100% yours and the privacy of your visitors is respected.',
