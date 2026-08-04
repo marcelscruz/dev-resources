98e2aa45341013a602f61da65191fb65c6c47373
Anka <runner@sjc22-bm210-54a43ebd-1e73-4683-b5ab-44013084c156-CEEF0F2F878B.local>
2026-08-04 19:21:10 +0000

    Auto updating db and README.md

--- resources/f.ts
+++ resources/f.ts
@@ -265,7 +265,7 @@ export const resources: Resource[] = [
             'SVG to WOFF2',
             'color glyphs',
             'monochrome icons',
-            'custom icons'
+            'custom icons',
         ],
     },
     {
